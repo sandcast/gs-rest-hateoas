d9d34cad7601649a7aaf76f571ccc2c1c2e693ef
Matthew Sandoz <matthew.sandoz@gmail.com>
2017-02-17 16:07:51 -0500

    2way

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -86,8 +86,9 @@ class Test implements EventListener {
         if (event instanceof MessageReceivedEvent) {
             MessageReceivedEvent e = (MessageReceivedEvent) event;
             System.out.println(e.getMessage().getContent());
-            if (!e.getAuthor().isBot())
-            e.getChannel().sendMessage("simon says " + e.getMessage().getContent());
+//            if (!e.getAuthor().isBot()) {
+                e.getChannel().sendMessage("simon says " + e.getMessage().getContent());
+//            }
         }
     }
 }
